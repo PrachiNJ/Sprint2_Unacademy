d22754d31d103b6034f7e2b033183f1870a9c266
varoo <varoo@LAPTOP-LPHN9A1C>
2021-01-07 17:56:52 +0530

    commit

---
+++ Unacademy/src/test/java/stepDefinitions/Un_StartlearningSteps.java
@@ -0,0 +1,48 @@
+package stepDefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Un_StartlearningSteps
+{
+	@Given("^user opens unacademy url$")
+    public void user_opens_unacademy_url() throws Throwable {
+        
+    }
+
+    @Given("^user is on exam module page$")
+    public void user_is_on_exam_module_page() throws Throwable {
+       
+    }
+
+    @When("^select start learning$")
+    public void select_start_learning() throws Throwable {
+        
+    }
+
+    @When("^user selects an exam$")
+    public void user_selects_an_exam() throws Throwable {
+        
+    }
+
+    @When("^user selects a course$")
+    public void user_selects_a_course() throws Throwable {
+        
+    }
+
+    @Then("^display exam module page$")
+    public void display_exam_module_page() throws Throwable {
+        
+    }
+
+    @Then("^display courses offered$")
+    public void display_courses_offered() throws Throwable {
+        
+    }
+
+    @Then("^displays subscription page$")
+    public void displays_subscription_page() throws Throwable {
+        
+    }
+}
--- Unacademy/src/test/java/testrunner/Un_testrunner.java
+++ Unacademy/src/test/java/testrunner/Un_testrunner.java
@@ -1,5 +1,17 @@
 package testrunner;
 
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = {"featurefiles/un_startlearning.feature"} /// which feature files to run
+		,glue={"pages"}				//package names having the scripts for the feature file
+		,tags= "@tag1,@tag2,@tag3"			// the tags to run.... which are in feature file		
+		)
+
 public class Un_testrunner {
 
 }
